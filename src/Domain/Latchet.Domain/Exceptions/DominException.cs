0e1c13c5a06370803c8b77741ef739bc9bba569d
SajadVahmi <sajad.vahmi@gmail.com>
2021-03-13 17:48:29 +0330

    add exceptions

---
+++ src/Domain/Latchet.Domain/Exceptions/DominException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Domain.Exceptions
+{
+    public abstract class DomainException : Exception
+    {
+        public string[] Parameters { get; set; }
+
+        public DomainException(string message, params string[] parameters) : base(message)
+        {
+            Parameters = parameters;
+        }
+    }
+}
---
+++ src/Domain/Latchet.Domain/Exceptions/InvalidValueObjectStateException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Domain.Exceptions
+{
+    public class InvalidValueObjectStateException : DomainException
+    {
+        public InvalidValueObjectStateException(string message, params string[] parameters) : base(message)
+        {
+            Parameters = parameters;
+        }
+    }
+}
