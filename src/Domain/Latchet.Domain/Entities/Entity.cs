71068aebec2db381be460a05c50e1248dc51ffd4
SajadVahmi <sajad.vahmi@gmail.com>
2021-03-13 19:18:38 +0330

    add entities

---
+++ src/Domain/Latchet.Domain/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using Latchet.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Domain.Entities
+{
+    public class Entity:IAuditable
+    {
+        public Id Id { get; set; }
+    }
+}
