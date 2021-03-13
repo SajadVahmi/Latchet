6b284e704a12ccd8c2bfe74682ac91478f2abdc9
SajadVahmi <sajad.vahmi@gmail.com>
2021-03-13 19:18:59 +0330

    add events

---
+++ src/Domain/Latchet.Domain/Events/IEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Domain.Events
+{
+    public interface IEvent
+    {
+        Guid EventId { get; }
+        DateTime PublishDateTime { get; }
+    }
+}
