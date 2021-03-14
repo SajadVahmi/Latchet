16886e0de05a231339d9b73752b14e07a4006d34
SajadVahmi <sajad.vahmi@gmail.com>
2021-03-14 17:13:49 +0330

    add utilities library

---
+++ src/Utilities/Latchet.Utilities/Users/IUserInfoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Utilities.Users
+{
+    public interface IUserInfoService
+    {
+        string GetUserAgent();
+        string GetUserIp();
+        int UserId();
+
+        string GetFirstName();
+        string GetLastName();
+        string GetUsername();
+        bool IsCurrentUser(string userId);
+        bool HasAccess(string accessKey);
+    }
+}
