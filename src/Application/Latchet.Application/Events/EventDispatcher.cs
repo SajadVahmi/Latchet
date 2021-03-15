d75308300f25efa16712cc83787cb45c346b383a
SajadVahmi <sajad.vahmi@gmail.com>
2021-03-15 19:21:57 +0330

    complete events contracts in application

---
+++ src/Application/Latchet.Application/Events/EventDispatcher.cs
@@ -0,0 +1,31 @@
+﻿using Latchet.Domain.Events;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Application.Events
+{
+    public class EventDispatcher : IEventDispatcher
+    {
+        private readonly IServiceScopeFactory serviceFactory;
+        public EventDispatcher(IServiceScopeFactory serviceScopeFactory)
+        {
+            serviceFactory = serviceScopeFactory;
+        }
+        #region Event Dispatcher
+        public async Task PublishDomainEventAsync<TDomainEvent>(TDomainEvent @event) where TDomainEvent : class, IDomainEvent
+        {
+            using var serviceProviderScop = serviceFactory.CreateScope();
+            var handlers = serviceProviderScop.ServiceProvider.GetServices<IDomainEventHandler<TDomainEvent>>();
+            foreach (var handler in handlers)
+            {
+                await handler.Handle(@event);
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ src/Application/Latchet.Application/Events/IEventDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using Latchet.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Application.Events
+{
+    public interface IEventDispatcher
+    {
+        Task PublishDomainEventAsync<TDomainEvent>(TDomainEvent @event) where TDomainEvent : class, IDomainEvent;
+
+    }
+}
