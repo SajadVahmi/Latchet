e9f1fc8239cd02730818c7dce0ec669dfe957a66
SajadVahmi <sajad.vahmi@gmail.com>
2021-03-16 08:25:51 +0330

    add query and query handler to applicaction

---
+++ src/Application/Latchet.Application/Queries/QueryHandler.cs
@@ -0,0 +1,57 @@
+﻿using Latchet.Application.Common;
+using Latchet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Application.Queries
+{
+    public interface IQueryHandler<TQuery, TData>
+        where TQuery : class, IQuery<TData>
+    {
+        Task<QueryResult<TData>> Handle(TQuery request);
+    }
+
+    public abstract class QueryHandler<TQuery, TData> : IQueryHandler<TQuery, TData>
+        where TQuery : class, IQuery<TData>
+    {
+        protected readonly LatchetServices latchetServices;
+        protected readonly QueryResult<TData> result = new QueryResult<TData>();
+
+        protected virtual Task<QueryResult<TData>> ResultAsync(TData data, ApplicationServiceStatus status)
+        {
+            result._data = data;
+            result.Status = status;
+            return Task.FromResult(result);
+        }
+
+        protected virtual QueryResult<TData> Result(TData data, ApplicationServiceStatus status)
+        {
+            result._data = data;
+            result.Status = status;
+            return result;
+        }
+
+
+        protected virtual Task<QueryResult<TData>> ResultAsync(TData data)
+        {
+            var status = data != null ? ApplicationServiceStatus.Ok : ApplicationServiceStatus.NotFound;
+            return ResultAsync(data, status);
+        }
+
+        protected virtual QueryResult<TData> Result(TData data)
+        {
+            var status = data != null ? ApplicationServiceStatus.Ok : ApplicationServiceStatus.NotFound;
+            return Result(data, status);
+        }
+
+        public QueryHandler(LatchetServices hamoonApplicationContext)
+        {
+            latchetServices = hamoonApplicationContext;
+        }
+
+        public abstract Task<QueryResult<TData>> Handle(TQuery request);
+    }
+}
---
+++ src/Application/Latchet.Application/Queries/QueryResult.cs
@@ -0,0 +1,21 @@
+﻿using Latchet.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Application.Queries
+{
+    public sealed class QueryResult<TData> : ApplicationServiceResult
+    {
+        internal TData _data;
+        public TData Data
+        {
+            get
+            {
+                return _data;
+            }
+        }
+    }
+}
