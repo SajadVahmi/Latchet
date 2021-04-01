7a7100acfedda18d10f586e86096a859b85c0c66
Sajad Vahmi <sajad.vahmi@gmail.com>
2021-04-01 11:24:07 +0430

    Move services to root

---
+++ src/Domain/Latchet.Domain/Services/AuthenticatedUserService/FakeAuthenticatedUserService.cs
@@ -0,0 +1,21 @@
+﻿using Latchet.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Domain.Services.AuthenticatedUserService
+{
+    public class FakeAuthenticatedUserService : IAuthenticatedUserService
+    {
+        public string GetAgent()=> "1";
+        public string GetFirstname()=> "Firstname";
+        public int GetId() => 1;
+        public string GetIp() => "0.0.0.0";
+        public string GetLastname() => "Lastname";
+        public string GetUsername() => "Username";
+        public bool HasAccess(string accessKey) => true;
+        public bool IsCurrentUser(string userId) => true;
+    }
+}
---
+++ src/Domain/Latchet.Domain/Services/ObjectMapper/IObjectMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Domain.Services.ObjectMappers
+{
+    public interface IObjectMapper
+    {
+        TDestination Map<TSource, TDestination>(TSource source);
+    }
+}
