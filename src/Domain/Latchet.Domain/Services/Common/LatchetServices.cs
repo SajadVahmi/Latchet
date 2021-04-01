ffae69510888f33ddd22df4d3f6056b15474b0b9
Sajad Vahmi <sajad.vahmi@gmail.com>
2021-04-01 13:16:25 +0430

    Add latchet services calss

---
+++ src/Domain/Latchet.Domain/Services/Common/LatchetServices.cs
@@ -0,0 +1,29 @@
+﻿using Latchet.Domain.Services.AuthenticatedUserService;
+using Latchet.Domain.Services.Caching;
+using Latchet.Domain.Services.JsonSerializer;
+using Latchet.Domain.Services.ObjectMappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Domain.Services.Common
+{
+    public class LatchetServices
+    {
+        public readonly ICacheProvider CacheProvider;
+        public readonly IObjectMapper ObjectMapper;
+        public readonly IJsonSerializer JsonSerializer;
+        public readonly IAuthenticatedUserService AuthenticatedUserService;
+
+        public LatchetServices(ICacheProvider cacheProvider, IObjectMapper objectMapper, 
+            IJsonSerializer jsonSerializer, IAuthenticatedUserService authenticatedUserService)
+        {
+            CacheProvider = cacheProvider;
+            ObjectMapper = objectMapper;
+            JsonSerializer = jsonSerializer;
+            AuthenticatedUserService = authenticatedUserService;
+        }
+    }
+}
