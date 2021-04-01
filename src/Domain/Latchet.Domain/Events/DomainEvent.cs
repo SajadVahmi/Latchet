b0508cbc18557e14b42b43c1e9b71665c95e4d1f
Sajad Vahmi <sajad.vahmi@gmail.com>
2021-04-01 11:24:48 +0430

    Add DomainEvent base class

---
+++ src/Domain/Latchet.Domain/Events/DomainEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Domain.Events
+{
+    public class DomainEvent : IDomainEvent
+    {
+        public Guid EventId { get; }
+        public DateTime PublishDateTime { get; }
+        public DomainEvent()
+        {
+            EventId = Guid.NewGuid();
+            PublishDateTime = DateTime.Now;
+        }
+    }
+}
