23e93e1a65132713ad4345016380c3bed707a325
Sajad Vahmi <sajad.vahmi@gmail.com>
2021-04-01 18:08:03 +0430

    Add event handler and event dispatcher to application layer

---
+++ src/Application/Latchet.Application/Events/EventDispatcher.cs
@@ -0,0 +1,30 @@
+﻿using Latchet.Domain.Events;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Application.Events
+{
+    public class EventDispatcher : IEventDispatcher
+    {
+        private readonly IServiceProvider serviceProvider;
+        public EventDispatcher(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        #region Event Dispatcher
+        public async Task PublishDomainEventAsync<TDomainEvent>(TDomainEvent @event) where TDomainEvent : class, IDomainEvent
+        {
+            var handlers = serviceProvider.GetServices<IDomainEventHandler<TDomainEvent>>();
+            foreach (var handler in handlers)
+            {
+                await handler.Handle(@event);
+            }
+        }
+        #endregion
+
+    }
+}
