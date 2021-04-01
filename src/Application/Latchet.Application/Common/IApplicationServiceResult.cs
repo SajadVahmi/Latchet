7b73aab7f01a8791ce4d6465e31b7807a59b23ca
Sajad Vahmi <sajad.vahmi@gmail.com>
2021-04-01 12:34:31 +0430

    Add application result models and contracts

---
+++ src/Application/Latchet.Application/Common/IApplicationServiceResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latchet.Application.Common
+{
+    public interface IApplicationServiceResult
+    {
+        IEnumerable<string> Messages { get; }
+        ApplicationServiceStatus Status { get; set; }
+    }
+}
