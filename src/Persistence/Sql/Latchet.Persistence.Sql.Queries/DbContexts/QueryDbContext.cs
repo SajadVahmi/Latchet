be25f7ccd0097d0c424ddda3eab9c3c3fe04800a
SajadVahmi <sajad.vahmi@gmail.com>
2021-08-08 15:09:55 +0430

    add query db context

---
+++ src/Persistence/Sql/Latchet.Persistence.Sql.Queries/DbContexts/QueryDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Latchet.Persistence.Sql.Queries.DbContexts
+{
+    public class QueryDbContext : DbContext
+    {
+        public QueryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public override int SaveChanges()
+        {
+            throw new NotSupportedException();
+        }
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            throw new NotSupportedException();
+
+        }
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            throw new NotSupportedException();
+
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotSupportedException();
+
+        }
+    }
+}
---
+++ src/Persistence/Sql/Latchet.Persistence.Sql.Queries/Repository/BaseQueryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Latchet.Domain.Data;
+using Latchet.Persistence.Sql.Queries.DbContexts;
+
+namespace Latchet.Persistence.Sql.Queries.Repository
+{
+    public class BaseQueryRepository<TDbContext> : IQueryRepository
+        where TDbContext : QueryDbContext
+    {
+        protected readonly TDbContext dbContext;
+        public BaseQueryRepository(TDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
